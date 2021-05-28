1598ca9cb7fc43ca3eb357133dbd650798ff8789
asoto92 <asoto92@gmail.com>
2021-05-28 12:28:13 -0500

    Cambios del 20210828

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  //styleUrls: ['./app.component.scss']
+  //styles: ['.container { color: blue; }']
 })
 export class AppComponent {
+  hello = 'hello';
   title = 'sygno-app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+/*  */
 @NgModule({
+  /* EL declarations SIGNIFICA QUE EN EL SE PONEN LOS MODULOS O DIRECTIVAS QUE SON PARTE DE ESTE MODULO (AppModule) */
   declarations: [
+    /* AppComponent ES UN MODULO  */
     AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+  /* EN exports SE INDICAN LOS MODULOS QUE PUEDEN SER REFERENCIADOS POR OTROS MODULOS */
+  exports: [AppComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
